adcae1dc4b258ba2abc4f209a71cbb29c37e7104
Ayan George <ayan@ayan.net>
2018-04-09 00:21:19 -0400

    Use go fmt
    Go provides a formatter that provides a standard way to indent code.
    
    It doesn't allow any options which makes all Go code look the same.  I
    may mangle it but the idea is that: "go fmt is everyone's favorite
    because it is noones favorite."
    
    Using go fmt settles any arguments about style and you'll find that it
    makes all Go code look familiar.
    
    I formatted all of the code in this directory with:
    
      $ go fmt ./...
    
    The tripple dots (ellipsis?) is a special notation that the tools use to
    indicate that it should recurse a directory.

--- client.go
+++ client.go
@@ -1,17 +1,17 @@
 package main
- 
+
 import (
-    "fmt"
-    "net"
-    "time"
+	"fmt"
+	"net"
+	"time"
 )
- 
+
 func CheckError(err error) {
-    if err  != nil {
-        fmt.Println("Error: " , err)
-    }
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
 }
- 
+
 /*
  * N1MM broadcasts Rotator commands from port 12040
  * Rotator status we send are sent from port 13010
@@ -19,28 +19,28 @@ func CheckError(err error) {
  */
 
 func main() {
-    ServerAddr,err := net.ResolveUDPAddr("udp","255.255.255.255:13010")
-    CheckError(err)
- 
-    LocalAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:")
-    CheckError(err)
- 
-    Conn, err := net.DialUDP("udp", LocalAddr, ServerAddr)
-    CheckError(err)
- 
-    defer Conn.Close()
-    i := 0
-    for {
-        msg := fmt.Sprintf("hexbeam-rotor@%d", i)
-        i+=2
-        if i > 365 {
-           i = 0
-        }
-        buf := []byte(msg)
-        _,err := Conn.Write(buf)
-        if err != nil {
-            fmt.Println(msg, err)
-        }
-        time.Sleep(time.Second * 1)
-    }
+	ServerAddr, err := net.ResolveUDPAddr("udp", "255.255.255.255:13010")
+	CheckError(err)
+
+	LocalAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:")
+	CheckError(err)
+
+	Conn, err := net.DialUDP("udp", LocalAddr, ServerAddr)
+	CheckError(err)
+
+	defer Conn.Close()
+	i := 0
+	for {
+		msg := fmt.Sprintf("hexbeam-rotor@%d", i)
+		i += 2
+		if i > 365 {
+			i = 0
+		}
+		buf := []byte(msg)
+		_, err := Conn.Write(buf)
+		if err != nil {
+			fmt.Println(msg, err)
+		}
+		time.Sleep(time.Second * 1)
+	}
 }
--- server-wconfig.go
+++ server-wconfig.go
@@ -5,88 +5,88 @@
  */
 
 package main
- 
+
 import (
-    "encoding/json"
-    "fmt"
-    "net"
-    "os"
+	"encoding/json"
+	"fmt"
+	"net"
+	"os"
 )
 
 type Configuration struct {
-    Rotators []map[string]string
-    AnotherThing string
+	Rotators     []map[string]string
+	AnotherThing string
 }
 
 func readConfig(jsonFileName string, rotorData map[string]map[string]string, otherData map[string]string) {
 
-file, err := os.Open(jsonFileName)
-    if err != nil {
-        fmt.Printf("Unable to open config file: <%s>\n", err)
-        os.Exit(1)
-    }
+	file, err := os.Open(jsonFileName)
+	if err != nil {
+		fmt.Printf("Unable to open config file: <%s>\n", err)
+		os.Exit(1)
+	}
 
-    defer file.Close()
-    decoder := json.NewDecoder(file)
-    configuration := Configuration{}
-    err = decoder.Decode(&configuration)
-    if err != nil {
-        fmt.Println("error1:", err)
-        os.Exit(1)
-    }
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	configuration := Configuration{}
+	err = decoder.Decode(&configuration)
+	if err != nil {
+		fmt.Println("error1:", err)
+		os.Exit(1)
+	}
 
-    for _, re := range configuration.Rotators {
-        var rname string
-        
-        if val, ok := re["name"]; ok {
-           rname = val
-         } else {
-           fmt.Printf("Rotator config found with no name defined: %v\n", re)
-           os.Exit(1)
-         }
-         // TODO if we have restirctions like no spaces in names, enforce it here
-         fmt.Printf("Rotor name: %s\n", rname)
+	for _, re := range configuration.Rotators {
+		var rname string
 
-         // Here I'll read the rest of the config parts, and put them in the rotorData map for use in main()
-    }
+		if val, ok := re["name"]; ok {
+			rname = val
+		} else {
+			fmt.Printf("Rotator config found with no name defined: %v\n", re)
+			os.Exit(1)
+		}
+		// TODO if we have restirctions like no spaces in names, enforce it here
+		fmt.Printf("Rotor name: %s\n", rname)
 
-    fmt.Printf("%s\n", configuration.AnotherThing)
+		// Here I'll read the rest of the config parts, and put them in the rotorData map for use in main()
+	}
+
+	fmt.Printf("%s\n", configuration.AnotherThing)
 }
 
 /* A Simple function to verify error */
 func CheckError(err error) {
-    if err  != nil {
-        fmt.Println("Error: " , err)
-        os.Exit(0)
-    }
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(0)
+	}
 }
 
 func main() {
-     var rotors map[string]map[string]string
-     var otherConfig map[string]string
+	var rotors map[string]map[string]string
+	var otherConfig map[string]string
 
-     readConfig("multirotorconf.json", rotors, otherConfig)
+	readConfig("multirotorconf.json", rotors, otherConfig)
 
-     // ignore the rest for now
-     os.Exit(0)
+	// ignore the rest for now
+	os.Exit(0)
 
-    /* Lets prepare a address to listen from any address sending at port 12040*/
-    ServerAddr,err := net.ResolveUDPAddr("udp",":12040")
-    CheckError(err)
- 
-    /* Now listen at selected port */
-    ServerConn, err := net.ListenUDP("udp", ServerAddr)
-    CheckError(err)
-    defer ServerConn.Close()
- 
-    buf := make([]byte, 1024)
- 
-    for {
-        n,addr,err := ServerConn.ReadFromUDP(buf)
-        fmt.Println("Received ",string(buf[0:n]), " from ",addr)
- 
-        if err != nil {
-            fmt.Println("Error: ",err)
-        } 
-    }
+	/* Lets prepare a address to listen from any address sending at port 12040*/
+	ServerAddr, err := net.ResolveUDPAddr("udp", ":12040")
+	CheckError(err)
+
+	/* Now listen at selected port */
+	ServerConn, err := net.ListenUDP("udp", ServerAddr)
+	CheckError(err)
+	defer ServerConn.Close()
+
+	buf := make([]byte, 1024)
+
+	for {
+		n, addr, err := ServerConn.ReadFromUDP(buf)
+		fmt.Println("Received ", string(buf[0:n]), " from ", addr)
+
+		if err != nil {
+			fmt.Println("Error: ", err)
+		}
+	}
 }
