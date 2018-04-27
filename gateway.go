8d01d565ee330b357af05aa3cef8da7649bc7672
Steve Conklin <steve@conklinhouse.com>
2018-04-27 10:56:29 -0500

    Restructure using goroutines, channels, and select

--- gateway.go
+++ gateway.go
@@ -5,43 +5,20 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
+	"time"
 )
 
-func getPosition(rotator config.Rotator) (position string, err error) {
-
-	cmd := fmt.Sprintf("/usr/bin/rotctl -m %s -r %s -s %s \\get_pos", rotator.Model, rotator.Port, rotator.PortSpeed)
-
-	out, err := exec.Command(cmd).Output()
-	if err != nil {
-		return "", err
-	}
-	result := string(out)
-	azimuth := strings.Split(result, "\n")[0]
-
-	return azimuth, err
-}
-
-/*
-func setPosition(rotator, position) (error) {
-}
-*/
-
-func dumpConfig(conf *config.Config) {
-	fmt.Printf("Rotators:\n")
-	for _, rotator := range conf.Rotators {
-		fmt.Printf("    Name:  %s\n", rotator.Name)
-		fmt.Printf("    Model: %s\n", rotator.Model)
-		fmt.Printf("    Port:  %s\n", rotator.Port)
-		fmt.Printf("    Speed: %s\n", rotator.PortSpeed)
-	}
-	fmt.Printf("Network:\n")
-	fmt.Printf("    Rotor Rx:  %s\n", conf.Network.RotorRx)
-	fmt.Printf("    Rotor Tx:  %s\n", conf.Network.RotorTx)
-	fmt.Printf("    Status Rx: %s\n", conf.Network.StatusRx)
+type Rinfo struct {
+	Azimuth float64
+	Name    string
 }
 
 func main() {
+	quit := make(chan bool)
+	errc := make(chan error)
+	pos := make(chan Rinfo)
 
 	conf, err := config.ReadConfig("rotorconf.json")
 
@@ -50,15 +27,63 @@ func main() {
 		os.Exit(1)
 	}
 
-	dumpConfig(conf)
+	/*config.DumpConfig(conf)*/
 
 	for _, rotator := range conf.Rotators {
-		position, err := getPosition(rotator)
-		if err != nil {
-			fmt.Println("Error: ", err)
-			os.Exit(1)
-		}
+		fmt.Print("For each rotator\n")
+		go func(rotator config.Rotator) {
+			tLast := time.Now()
+			var posLast float64 = 0.0
+			var deltap float64 = 0.0
+			var azI float64 = 0.0
 
-		fmt.Printf("Name: %s, Azimuth: %s\n ", rotator.Name, position)
+			for {
+				cmdargs := fmt.Sprintf("/usr/bin/rotctl -m %s -r %s -s %s get_pos", rotator.Model, rotator.Port, rotator.PortSpeed)
+				out, err := exec.Command("bash", "-c", cmdargs).Output()
+				if err != nil {
+					fmt.Println(err)
+					errc <- err
+				} else {
+					result := string(out)
+					azimuth := strings.Split(result, "\n")[0]
+					azI, err = strconv.ParseFloat(azimuth, 64)
+					if err != nil {
+						errc <- err
+					}
+					deltap = azI - posLast
+					if deltap < 0 {
+						deltap = deltap * -1
+					}
+
+					if (deltap > 1) || (time.Now().Sub(tLast) > (15 * time.Second)) {
+						pos <- Rinfo{azI, rotator.Name}
+						posLast = azI
+						tLast = time.Now()
+					}
+				}
+				select {
+				case <-errc:
+					return
+				case <-quit:
+					return
+				default:
+				}
+				time.Sleep(1000 * time.Millisecond)
+			}
+		}(rotator)
+	}
+
+	for {
+		select {
+		case <-errc:
+			fmt.Printf("Quitting . . . \n")
+			close(quit)
+			return
+		case p := <-pos:
+			fmt.Printf("MAIN LOOP Rotor <%s> Position: %f\n", p.Name, p.Azimuth)
+			/* Send the UDP packet with rotator position */
+			/* first see if we have a conflicting name with a received value? */
+			/*default: */
+		}
 	}
 }
