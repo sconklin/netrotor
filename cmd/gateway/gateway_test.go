b9cd7f3d1e5122e9f4416a019862dbe08c1e5151
Steve Conklin <steve@conklinhouse.com>
2019-07-14 18:07:01 -0500

    fix test files and add gitignore

---
+++ cmd/gateway/gateway_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestGateway(t *testing.T) {
+	// t.Fatal("not implemented")
+
+	// Add some tests
+	/*
+		_, err := ReadConfig("test-fixtures/multirotorconf.json")
+
+		if err != nil {
+			t.Fatalf("readConfig() returned unexpected error: %v", err)
+		}
+	*/
+}
--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -7,7 +7,7 @@ import (
 func TestConfig(t *testing.T) {
 	// t.Fatal("not implemented")
 
-	_, err := readConfig("test-fixtures/multirotorconf.json")
+	_, err := ReadConfig("test-fixtures/multirotorconf.json")
 
 	if err != nil {
 		t.Fatalf("readConfig() returned unexpected error: %v", err)
